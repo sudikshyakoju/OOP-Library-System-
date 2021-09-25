91d9bb3b3b8545136a13f7c9822fa2bac450166c
sudikshyakoju <sudikoju@gmail.com>
2021-09-25 15:01:53 +0545

    library management system

--- src/testing/register_classTest.java
+++ src/testing/register_classTest.java
@@ -9,14 +9,14 @@ import org.junit.jupiter.api.Test;
 class register_classTest extends register_class{
 
 	@Test
-	// testing class not null
+	// testing class - not null
 	public void testConstructor() {
 		register_class rtc = new register_class();
 		assertNotNull(rtc);
 	}
 	
 	@Test
-	// Register inserting test pass
+	// Register inserting test - pass
 	public void testGetters() {
 		register_class rt = new register_class("sudikshya","sudi","04/11/2002","female", "sudikshya11");
 		String expected = "sudi";
@@ -26,7 +26,7 @@ class register_classTest extends register_class{
 	}
 	
 	     @Test
-	    // Register inserting test fail
+	    // Register inserting test - fail
 		public void testGetters1() {
 			register_class rt = new register_class("sudikshya","sudi","04/11/2002","female", "sudikshya11");
 			String expected = "sudikshya";
